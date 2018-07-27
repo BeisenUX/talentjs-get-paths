6712a76873d9cfdc7c3857efe194150c0443ea47
yuanyuan <yuanyuan@yuanyuandeMacBook-Pro.local>
2018-07-27 18:36:22 +0800

    添加模板路径解析

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-
 const acorn = require('acorn')
 const walk = require('acorn/dist/walk')
 
 export default async (content) => {
   let depPaths = await new Promise((resolve, reject) => {
+    let paths = [], templatePaths = []
     walk.simple(
       acorn.parse(content),
       {
         ExpressionStatement (node) {
           // 判断是不是define
+          // console.log(node)
           if (node.expression.type === 'CallExpression' &&
               node.expression.callee.type === 'Identifier' &&
               node.expression.callee.name === 'define') {
             // 获取以来模块路径
             let [ arrayExpression ] = node.expression.arguments
-            let paths = arrayExpression.elements.map(pathNode => pathNode.value)
-            resolve(paths)
+            paths = arrayExpression.elements.map(pathNode => pathNode.value)
+          }
+        },
+        MemberExpression (node) {
+          if(node.object && node.object.name === 'jst') {
+            templatePaths.push(node.property.value)
           }
         }
       }
     )
+    resolve({
+      paths,
+      templatePaths
+    })
   })
   return depPaths
 }
